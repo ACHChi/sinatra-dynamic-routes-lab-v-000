974078f8526a2fff8e3c8773f79a2596f8c06ea4
Chao Hsuan Chi <allen0413@gmail.com>
2019-08-28 06:55:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,8 +17,10 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @num = params[:number].to_i
     @phrase = params[:phrase]
-    @fin = @phrase.times.@num
-    "#{@fin}"
+    @num.times do
+      @fin += @phrase
+    end
+    @fin
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
